1365c6bda685ffbad4db1a819ec4c4d99353d1f0
Henry Alexandre de Almeida Frungilo <henryfrungilo@outlook.com>
2024-06-11 08:36:37 -0300

    feat[major]: implement login page

---
+++ VIEW/tela-login.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Zenith - Luxury Car Manager</title>
+    <link rel="stylesheet" href="./CSS/login.css">
+</head>
+<body>
+    
+    <h1>Zenith</h1>
+    <small>Luxury car manager</small>
+    <form 
+    method="post" 
+    action="" 
+    class="card col align-center"
+
+    >
+        <h2>Login</h2>
+
+        <div class="col">
+            <label for="input-email">Email</label>
+            <input id="input-email" name="input-email" type="email">
+        </div>
+
+        <div class="col">
+            <label for="input-senha">Senha</label>
+            <input id="input-senha" name="input-senha" type="password">
+        </div>
+
+        <button
+        class="button"
+        type="submit"
+        > Login </button>      
+
+    </form>
+</body>
+</html>
