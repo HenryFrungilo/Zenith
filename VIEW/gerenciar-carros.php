05c0610620a2a80d5fafa9e7ffbd40e5da407aba
Henry Alexandre de Almeida Frungilo <henryfrungilo@outlook.com>
2024-06-12 10:44:29 -0300

    feat: add car manager page

---
+++ VIEW/gerenciar-carros.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Zenith - Editar carros de Pagani</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="./assets/CSS/gerenciar-carros.css">
+</head>
+
+<body>
+    <div class="container-fluid">
+        <div class="header">
+            <div class="titleContent row text-center">
+                <h1>Zenith</h1>
+                <small>
+                    Your Luxury Car Manager
+                </small>
+            </div>
+        </div>
+        <div class="content">
+            <div class="ladoEsquerdo">
+                <table>
+                    <tr>
+                        <th class="text-center">
+                            <h2 aria-colspan="5">PAGANI</h2>
+                        </th>
+                    </tr>
+                    <td>
+                        <tr class="">
+                            <th>Modelo</th>
+                            <th>Cor</th>
+                            <th>Preço</th>
+                            <th>Quantidade</th>
+                            <th>Editar</th>
+                        </tr>
+                        <tr class="text-center">
+                            <td>Huayra</td>
+                            <td>Preto</td>
+                            <td>R$ 5.000.000,00</td>
+                            <td>5</td>
+                            <td><a href="#">Editar</a></td>
+                        </tr>
+                        <tr class="text-center">
+                            <td>Zonda</td>
+                            <td>Vermelho</td>
+                            <td>R$ 4.000.000,00</td>
+                            <td>3</td>
+                            <td><a href="#">Editar</a></td>
+                        </tr>
+                        <tr class="text-center">
+                            <td>Imola</td>
+                            <td>Prata</td>
+                            <td>R$ 6.000.000,00</td>
+                            <td>2</td>
+                            <td><a href="#">Editar</a></td>
+                        </tr>
+                        <tr class="text-center">
+                            <td>Tricolore</td>
+                            <td>Verde</td>
+                            <td>R$ 7.000.000,00</td>
+                            <td>1</td>
+                            <td><a href="#">Editar</a></td>
+                        </tr>
+                    </td>
+                </table>
+            </div>
+            <div class="ladoDireito">
+
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
