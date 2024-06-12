9ad1d299cdb1f788ecbee390d599afc307d414a0
Henry Alexandre de Almeida Frungilo <henryfrungilo@outlook.com>
2024-06-11 22:10:05 -0300

    feat: implement main page

---
+++ VIEW/pagina-principal.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Zenith - Koenigsegg</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="./assets/CSS/tela-principal.css">
+</head>
+
+<body class="">
+    <video autoplay muted loop id="backgroundVideo">
+        <source src="./assets/VID/koenigsegg-background.mp4" type="video/mp4">
+        Your browser does not support HTML5 video.
+    </video>
+    <div class="diagonalBackground"></div>
+    <div class="container-fluid">
+        <div class="" id="welcome-text">
+            <span>Bem vindo,</span>
+            <p>
+                Christian Von Koenigsegg
+            </p>
+        </div>
+        <div class="">
+            <ul>
+                <li>
+                    Dono de <b>Koenigsegg</b>
+                </li>
+                <li>
+                    Modelos Ativos
+                </li>
+                <ul>
+                    <li>
+                        <b>Gemera</b>
+                    </li>
+                    <li>
+                        <b>Jesko</b>
+                    </li>
+                    <li>
+                        <b>Regera</b>
+                    </li>
+                </ul>
+                <li>
+                    <a href="#" class="gerenciar-modelos-text">Gerenciar Modelos Ativos</a>
+                </li>
+            </ul>
+            <div class="lado-direito logo-marca">
+                <img src="./assets/IMAGES/koenigsegg-logo.png" alt="Koenigsegg">
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
