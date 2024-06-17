8bb6a55f5e80cd6b14377a7c9a24f126ab48e569
Henry Alexandre de Almeida Frungilo <henryfrungilo@outlook.com>
2024-06-17 16:44:34 -0300

    refactor: add modal js

---
+++ VIEW/assets/JS/modal.js
@@ -0,0 +1,6 @@
+const modal = document.getElementById('modalEditarCarro')
+const input = document.getElementById('inputEditarCarro')
+
+myModal.addEventListener('shown.bs.modal', () => {
+  myInput.focus()
+})
