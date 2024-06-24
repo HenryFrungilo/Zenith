2d439f38b39916e45f121b39a27a430eb8e4707a
Henry Alexandre de Almeida Frungilo <henryfrungilo@outlook.com>
2024-06-24 13:07:43 -0300

    fix: fix minor text problems

--- VIEW/gerenciar-carros.php
+++ VIEW/gerenciar-carros.php
@@ -68,7 +68,7 @@
                         </td>
                         <tr class="addCarro">
                             <td colspan="5" class="botaoAddCarro text-center">
-                                <button type="button" class="botaoAdicionarCarro btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">Add car</button>
+                                <button type="button" class="botaoAdicionarCarro btn btn-primary" data-bs-toggle="modal" data-bs-target="#editionModal">Add car</button>
                             </td>
                         </tr>
                     </table>
@@ -83,25 +83,9 @@
             </div>
         </div>
     </div>
-    <!-- Modal -->
-    <div class="modal" tabindex="-1" id="exampleModal">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title">Modal title</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <p>Modal body text goes here.</p>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary">Save changes</button>
-                </div>
-            </div>
-        </div>
-    </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
 </body>
 
 </html>
